e1ff180811aed43b9ed31f984de0a41c4c64a92e
PranaykumarManepally1998 <pranay.manepally@shipyaari.com>
2024-10-14 18:47:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Next from "./Next";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      hello mahavir
+      <button>Next</button>
+      <Next />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Next.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Next = () => {
+  return <div>hello pranay</div>;
+};
+
+export default Next;
